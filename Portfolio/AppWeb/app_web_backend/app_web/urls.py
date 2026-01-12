12868c6337046a44a8eb145516f3f47ed31386c0
LycanSilvermane <redx_94@hotmail.com>
2026-01-12 18:27:11 -0500

    last update to portfolio

--- Portfolio/AppWeb/app_web_backend/app_web/urls.py
+++ Portfolio/AppWeb/app_web_backend/app_web/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
     path('api/', include(router.urls)),
-        path('api/', include('users.urls')),                 
+    path('api/', include('users.urls')),                 
 ]
 
